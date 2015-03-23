bed69fc154358e5483fdf9f332a890fe681cff8c
garygg42 <garygg42@gmail.com>
2015-03-23 23:30:20 +0200

    added tests

---
+++ springboot-jersey/src/test/java/com/ig/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.ig.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.ig" })
+public class TestConfig {
+}
