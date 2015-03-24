a350d51d5ba3013ed3834622bc51665e1f64f4f3
gary <garygg42@gmail.com>
2015-03-24 15:10:47 +0200

    updated tests
    updated tests

--- springboot-jersey/src/test/java/com/ig/config/TestConfig.java
+++ springboot-jersey/src/test/java/com/ig/config/TestConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "com.ig" })
+@ComponentScan(basePackages = { "com.ig.rest" })
 public class TestConfig {
 }
---
+++ springboot-jersey/src/test/java/com/ig/test/CustomerResourceTest.java
@@ -0,0 +1,40 @@
+package com.ig.test;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ig.JerseyConfig;
+import com.ig.config.TestConfig;
+
+public class CustomerResourceTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		ApplicationContext context = new AnnotationConfigApplicationContext(
+				TestConfig.class);
+		return new JerseyConfig().property("contextConfig", context);
+	}
+
+	@Test
+	public void testFindCustomerWithId1() throws JSONException {
+		String customer = target("customers").path("1").request()
+				.get(String.class);
+		String expected = "{\"firstName\":\"Jack\",\"lastName\":\"Bauer\"}";
+		JSONAssert.assertEquals(expected, customer, JSONCompareMode.LENIENT);
+	}
+
+	@Test
+	public void testFindJackBauers() throws JSONException {
+		String customers = target("customers").queryParam("firstName", "Jack")
+				.queryParam("lastName", "Bauer").request().get(String.class);
+		String expected = "[{\"firstName\":\"Jack\",\"lastName\":\"Bauer\"},{\"firstName\":\"Jack\",\"lastName\":\"Bauer\"}]";
+		JSONAssert.assertEquals(expected, customers, JSONCompareMode.LENIENT);
+	}
+}
